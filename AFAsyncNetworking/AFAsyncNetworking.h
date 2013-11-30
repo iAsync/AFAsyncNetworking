c0c9ff82cf0b81491c7166c90925a955880e5cb2
dodikk <dodikk88.reg@gmail.com>
2013-11-30 17:23:01 +0200

    Initial commit

---
+++ AFAsyncNetworking/AFAsyncNetworking.h
@@ -0,0 +1,13 @@
+//
+//  AFAsyncNetworking.h
+//  AFAsyncNetworking
+//
+//  Created by Oleksandr Dodatko on 30/11/2013.
+//  Copyright (c) 2013 iAsync. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFAsyncNetworking : NSObject
+
+@end
---
+++ AFAsyncNetworking/AFAsyncNetworking.m
@@ -0,0 +1,13 @@
+//
+//  AFAsyncNetworking.m
+//  AFAsyncNetworking
+//
+//  Created by Oleksandr Dodatko on 30/11/2013.
+//  Copyright (c) 2013 iAsync. All rights reserved.
+//
+
+#import "AFAsyncNetworking.h"
+
+@implementation AFAsyncNetworking
+
+@end
