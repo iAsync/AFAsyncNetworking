cca215d4353da8209cfbee0dc6e88f399fced958
dodikk <dodikk88.reg@gmail.com>
2013-12-01 21:26:08 +0200

    Added download and upload task builders

--- AFAsyncNetworking/AFAsyncOperationFactory.h
+++ AFAsyncNetworking/AFAsyncOperationFactory.h
@@ -1,3 +1,5 @@
+#import <AFAsyncNetworking/AFNetworkingBlocks.h>
+
 #import <JFFAsyncOperations/JFFAsyncOperations.h>
 #import <Foundation/Foundation.h>
 
@@ -6,9 +8,40 @@
 
 @interface AFAsyncOperationFactory : NSObject
 
+
+#pragma mark - 
+#pragma mark NSUrlConnection based
 +(JFFAsyncOperation)asyncOperationFromAFHTTPRequestOperation:( AFHTTPRequestOperation* )afOperation;
 
-+(JFFAsyncOperation)asyncDataTaskOperationFromRequest:( NSURLRequest*) request
+
+#pragma mark -
+#pragma mark DataTask
++(JFFAsyncOperation)asyncDataTaskOperationFromRequest:( NSURLRequest* )request
                                            andSession:( AFHTTPSessionManager* )sessionManager;
+
+#pragma mark -
+#pragma mark DownloadTask
++(JFFAsyncOperation)asyncDownloadTaskOperationFromRequest:( NSURLRequest* )request
+                                               andSession:( AFHTTPSessionManager* )sessionManager
+                            usingOptionalTmpFileNameBlock:( AFTmpFileNameBuilderBlock )tmpFileNameBuilder;
+
++(JFFAsyncOperation)asyncDownloadTaskOperationFromResumeData:( NSData*)resumeData
+                                                  andSession:( AFHTTPSessionManager* )sessionManager
+                               usingOptionalTmpFileNameBlock:( AFTmpFileNameBuilderBlock )tmpFileNameBuilder;
+
+
+#pragma mark -
+#pragma mark UploadTask
++(JFFAsyncOperation)asyncUploadTaskOperationWithFile:( NSURL* )urlOfFileWithUploadData
+                                             request:( NSURLRequest* )request
+                                             session:( AFHTTPSessionManager* )sessionManager;
+
++(JFFAsyncOperation)asyncUploadTaskOperationWithData:( NSData* )uploadData
+                                             request:( NSURLRequest* )request
+                                             session:( AFHTTPSessionManager* )sessionManager;
+
++(JFFAsyncOperation)asyncUploadTaskOperationWithStreamedRequest:( NSURLRequest* )request
+                                                        session:( AFHTTPSessionManager* )sessionManager;
+
 
 @end
--- AFAsyncNetworking/AFAsyncOperationFactory.m
+++ AFAsyncNetworking/AFAsyncOperationFactory.m
@@ -1,11 +1,18 @@
 #import "AFAsyncOperationFactory.h"
 
 
-#import "AFAbstractTaskAdapter.h"
+#import "AFDataTaskAdapter.h"
+#import "AFUploadTaskAdapter.h"
+#import "AFDownloadTaskAdapter.h"
+
+
 #import "AFURLConnectionOperationAdapter.h"
 
 @implementation AFAsyncOperationFactory
 
+
+#pragma mark -
+#pragma mark NSUrlConnection based
 +(JFFAsyncOperation)asyncOperationFromAFHTTPRequestOperation:( AFHTTPRequestOperation* )afOperation
 {
    JFFAsyncOperationInstanceBuilder factory = ^id<JFFAsyncOperationInterface>
@@ -17,17 +24,116 @@
    return buildAsyncOperationWithAdapterFactory( factory );
 }
 
+
+#pragma mark -
+#pragma mark DataTask
 +(JFFAsyncOperation)asyncDataTaskOperationFromRequest:( NSURLRequest*) request
                                            andSession:( AFHTTPSessionManager* )sessionManager
 {
    JFFAsyncOperationInstanceBuilder factory = ^id<JFFAsyncOperationInterface>
    {
-      AFAbstractTaskAdapter* adapter =
-         [ [ AFAbstractTaskAdapter alloc ] initWithRequest: request
-                                      AFHTTPSessionManager: sessionManager ];
+      AFDataTaskAdapter* adapter =
+      [ [ AFDataTaskAdapter alloc ] initWithRequest: request
+                               AFHTTPSessionManager: sessionManager ];
       return adapter;
    };
 
+   return buildAsyncOperationWithAdapterFactory( factory );
+}
+
+
+
+#pragma mark -
+#pragma mark DownloadTask
++(JFFAsyncOperation)asyncDownloadTaskOperationFromRequest:( NSURLRequest*) request
+                                      andSession:( AFHTTPSessionManager* )sessionManager
+                   usingOptionalTmpFileNameBlock:( AFTmpFileNameBuilderBlock )tmpFileNameBuilder
+{
+   tmpFileNameBuilder = [ tmpFileNameBuilder copy ];
+   
+   JFFAsyncOperationInstanceBuilder factory = ^id<JFFAsyncOperationInterface>
+   {
+      AFDownloadTaskAdapter* adapter =
+      [ [ AFDownloadTaskAdapter alloc ] initWithRequest: request
+                                   AFHTTPSessionManager: sessionManager
+                                       tmpFileNameBlock: tmpFileNameBuilder ];
+      return adapter;
+   };
+   
+   return buildAsyncOperationWithAdapterFactory( factory );
+}
+
++(JFFAsyncOperation)asyncDownloadTaskOperationFromResumeData:( NSData*)resumeData
+                                         andSession:( AFHTTPSessionManager* )sessionManager
+                      usingOptionalTmpFileNameBlock:( AFTmpFileNameBuilderBlock )tmpFileNameBuilder
+{
+   tmpFileNameBuilder = [ tmpFileNameBuilder copy ];
+   
+   JFFAsyncOperationInstanceBuilder factory = ^id<JFFAsyncOperationInterface>
+   {
+      AFDownloadTaskAdapter* adapter =
+      [ [ AFDownloadTaskAdapter alloc ] initWithResumeData: resumeData
+                                      AFHTTPSessionManager: sessionManager
+                                          tmpFileNameBlock: tmpFileNameBuilder ];
+      
+      return adapter;
+   };
+
+   return buildAsyncOperationWithAdapterFactory( factory );
+}
+
+
+#pragma mark -
+#pragma mark UploadTask
++(JFFAsyncOperation)asyncUploadTaskOperationWithFile:( NSURL* )urlOfFileWithUploadData
+                                             request:( NSURLRequest* )request
+                                             session:( AFHTTPSessionManager* )sessionManager
+{
+   JFFAsyncOperationInstanceBuilder factory = ^id<JFFAsyncOperationInterface>
+   {
+
+      AFUploadTaskAdapter* adapter =
+      [ [ AFUploadTaskAdapter alloc ] initWithRequest: request
+                            urlOfFileWithDataToUpload: urlOfFileWithUploadData
+                                 AFHTTPSessionManager: sessionManager ];
+
+      return adapter;
+   };
+   
+   return buildAsyncOperationWithAdapterFactory( factory );
+}
+
++(JFFAsyncOperation)asyncUploadTaskOperationWithData:( NSData* )uploadData
+                                             request:( NSURLRequest* )request
+                                             session:( AFHTTPSessionManager* )sessionManager
+{
+   JFFAsyncOperationInstanceBuilder factory = ^id<JFFAsyncOperationInterface>
+   {
+      
+      AFUploadTaskAdapter* adapter =
+      [ [ AFUploadTaskAdapter alloc ] initWithRequest: request
+                                         dataToUpload: uploadData
+                                 AFHTTPSessionManager: sessionManager ];
+      
+      return adapter;
+   };
+   
+   return buildAsyncOperationWithAdapterFactory( factory );
+}
+
++(JFFAsyncOperation)asyncUploadTaskOperationWithStreamedRequest:( NSURLRequest* )request
+                                                        session:( AFHTTPSessionManager* )sessionManager
+{
+   JFFAsyncOperationInstanceBuilder factory = ^id<JFFAsyncOperationInterface>
+   {
+      
+      AFUploadTaskAdapter* adapter =
+      [ [ AFUploadTaskAdapter alloc ] initWithStreamedRequest: request
+                                         AFHTTPSessionManager: sessionManager ];
+      
+      return adapter;
+   };
+   
    return buildAsyncOperationWithAdapterFactory( factory );
 }
 
