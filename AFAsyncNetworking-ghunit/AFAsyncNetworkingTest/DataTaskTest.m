96814d4b6c58beb80966b6c297a5a41dd019b1f7
dodikk <dodikk88.reg@gmail.com>
2013-12-07 21:45:00 +0200

    Added unit test for DataTask

---
+++ AFAsyncNetworking-ghunit/AFAsyncNetworkingTest/DataTaskTest.m
@@ -0,0 +1,85 @@
+@interface DataTaskTest : GHAsyncTestCase
+
+@end
+
+@implementation DataTaskTest
+{
+   NSURLRequest              * _request;
+   NSData                    * _expectedReadme;
+   
+   NSURLSessionConfiguration * _config;
+   NSOperationQueue          * _mainQueue;
+   
+   AFHTTPSessionManager      * _afSessionManager;
+}
+
+
+-(void)setUp
+{
+   [ super setUp ];
+   
+   NSURL* url = [ NSURL URLWithString: @"https://raw.github.com/iAsync/AFAsyncNetworking/master/README.md" ];
+   NSURLRequest* request = [ NSURLRequest requestWithURL: url ];
+   self->_request = request;
+   
+   NSString* readmePath = [ [ NSBundle mainBundle ] pathForResource: @"README"
+                                                             ofType: @"md" ];
+   self->_expectedReadme = [ NSData dataWithContentsOfFile: readmePath ];
+   
+   
+   self->_config = [ NSURLSessionConfiguration defaultSessionConfiguration ];
+   self->_mainQueue = [ NSOperationQueue mainQueue ];
+   
+   self->_afSessionManager = [ AFHTTPSessionManager manager ];
+   self->_afSessionManager.responseSerializer = [ AFHTTPResponseSerializer serializer ];
+   
+}
+
+-(void)tearDown
+{
+   self->_request          = nil;
+   self->_expectedReadme   = nil;
+   self->_config           = nil;
+   self->_mainQueue        = nil;
+   self->_afSessionManager = nil;
+   
+   
+   [ super tearDown ];
+}
+
+-(void)testReadmeIsDownloadedCorrectly
+{
+   __block AFDataTaskCompletionInfo* received = nil;
+   
+   __block NSData* receivedReadme = nil;
+   __block NSError* error = nil;
+   
+   SEL testMethod = _cmd;
+   [ self prepare: testMethod ];
+
+   JFFDidFinishAsyncOperationHandler completionBlock = ^void( AFDataTaskCompletionInfo* blockResult, NSError* blockError )
+   {
+      error = blockError;
+      received = blockResult;
+      
+      [ self notify: kGHUnitWaitStatusSuccess
+        forSelector: testMethod ];
+   };
+   
+   JFFAsyncOperation loader =
+   [ AFAsyncOperationFactory asyncDataTaskOperationFromRequest: self->_request
+                                                    andSession: self->_afSessionManager ];
+   loader( nil, nil, completionBlock );
+
+   
+   [ self waitForStatus: kGHUnitWaitStatusSuccess
+                timeout: 1000 ];
+   GHAssertTrue( [ received isMemberOfClass: [ AFDataTaskCompletionInfo class ] ], @"data task result class mismatch" );
+   
+   receivedReadme = received.responseObject;
+   GHAssertTrue( [receivedReadme isKindOfClass: [ NSData class ] ], @"response object class mismatch" );
+   
+   GHAssertTrue( [ self->_expectedReadme isEqualToData: receivedReadme ], @"downloaded content mismatch" );
+}
+
+@end
---
+++ AFAsyncNetworking-ghunit/AFAsyncNetworkingTestTests/AFAsyncNetworkingTestTests.m
@@ -0,0 +1,34 @@
+//
+//  AFAsyncNetworkingTestTests.m
+//  AFAsyncNetworkingTestTests
+//
+//  Created by Oleksandr Dodatko on 07/12/2013.
+//  Copyright (c) 2013 iAsync. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AFAsyncNetworkingTestTests : XCTestCase
+
+@end
+
+@implementation AFAsyncNetworkingTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
